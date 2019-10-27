c4b2182efaeca156cea5ffbe605f289aa1b65765
stera10 <stera10@live.jp>
2019-10-28 03:37:46 +0900

    update files

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,24 @@
-
-// import files
-// -------------------------------------------
+// imports
+// ====================
+import * as $ from 'jquery';
 import * as THREE from 'three';
-import Stats from 'three/examples/jsm/libs/stats.module.js';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { SVGLoader } from 'three/examples/jsm/loaders/SVGLoader.js';
-import { GeometryUtils } from 'three/examples/jsm/utils/GeometryUtils.js';
+import { TweenMax } from 'gsap';
+
+// tab contents
+// =====================
+$(function () {
+  // 初期表示
+  $('.tabContents').hide();
+  $('.tabContents').eq(0).show();
+  $('[tab-item]').eq(0).addClass('active');
+  // jsTabItemどれかがクリックされたら該当するページを表示する
+  $('[tab-item]').click(function () {
+    var index = $('[tab-item]').index(this);
+    //現在表示されているタブ、内容を隠す
+    $('.tabContents').hide();
+    $('[tab-item]').removeClass('active');
+    // 選択されたタブ、内容を表示する
+    $('.tabContents').eq(index).show();
+    $('[tab-item]').eq(index).addClass('active');
+  });
+});
